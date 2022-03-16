b867be9805234f077a9e62654de7e53cd29837ea
CJAllen056 <christopherjallen056@gmail.com>
2022-03-16 20:34:38 +0000

    begun using XYChart from visx. Getting list of all top topics from data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
+import { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
+import { XYChart, Axis, Grid, LineSeries } from "@visx/xychart";
 
 import { posts } from "./queries";
 
+const getTopics = (topics, updateTopics) => {
+
+}
+
 const App = () => {
   const { loading, error, data } = useQuery(posts(1000));
+  const { graphData, updateData } = useState({});
+  const topics = []
 
   console.log(data);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error</p>;
+
+  data.posts.forEach(d => {
+    const topic = d.likelyTopics[0].label;
+
+    if (!topics[topic]) {
+      topics[topic] = 1;
+    } else {
+      topics[topic]++;
+    }
+  });
+
+  console.log(topics);
 
   return (
-    <ul>
-      {
-        data.posts.map(post => {
-          const date = new Date();
-          date.setTime(post.createdAt)
-
-          return(<li key={post.id}>{date.toLocaleString()}</li>)
-        })
-      }
-    </ul>
+    <div>
+      <XYChart height={300} xScale={{ type: "time" }} yScale={{ type: "linear" }}>
+        <Axis orientation="bottom" />
+        <Axis orientation="left" />
+        {/*<LineSeries data={} />*/}
+      </XYChart>
+      <ul>
+        {
+          data.posts.map(post => {
+            const date = new Date();
+            date.setTime(post.createdAt)
+  
+            return(<li key={post.id}>{date.toLocaleString()}</li>)
+          })
+        }
+      </ul>
+    </div>
   );
 }
 
