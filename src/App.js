64bfd4ff06c2cc6a781e2f6f70358176fdf19f6d
CJAllen056 <christopherjallen056@gmail.com>
2022-03-16 19:44:18 +0000

    playing around getting data to display

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { gql, useQuery } from "@apollo/client";
 import { posts } from "./queries";
 
 const App = () => {
-  const { loading, error, data } = useQuery(posts(500));
+  const { loading, error, data } = useQuery(posts(1000));
 
   console.log(data);
-  
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
     <ul>
       {
-        data.posts.map((post) => {
-          return(<li>{Date(post.createdAt)}</li>)
+        data.posts.map(post => {
+          const date = new Date();
+          date.setTime(post.createdAt)
+
+          return(<li key={post.id}>{date.toLocaleString()}</li>)
         })
       }
     </ul>
