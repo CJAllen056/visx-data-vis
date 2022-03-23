8f222c77c93a8c9b84ef8426940a567dccc40ad4
CJAllen056 <christopherjallen056@gmail.com>
2022-03-23 18:59:25 +0000

    importing css file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { gql, useQuery } from "@apollo/client";
 import Chart from './chart';
 
 import { posts } from "./queries";
+import './app.css';
 
 const App = () => {
   const { loading, error, data } = useQuery(posts(1000));
