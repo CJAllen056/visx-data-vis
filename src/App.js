7cdb12cf88cb139fce231cb10454bc5748171575
CJAllen056 <christopherjallen056@gmail.com>
2022-03-23 15:35:40 +0000

    switched to animated cuz why not

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
-import { XYChart, Axis, Grid, LineSeries } from "@visx/xychart";
+import { XYChart, AnimatedAxis, AnimatedGrid, AnimatedLineSeries } from "@visx/xychart";
 
 import { posts } from "./queries";
-
-const getTopics = (topics, updateTopics) => {
-
-}
 
 const formatData = (data) => {
   const dates = [
@@ -37,35 +33,22 @@ const App = () => {
   const { loading, error, data } = useQuery(posts(1000));
   const [graphData, setGraphData] = useState([]);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
   const accessors = {
     xAccessor: (d) => new Date(`${d.x} 2019`),
     yAccessor: (d) => d.y,
   };
 
-  console.log(data);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
-
-  console.log(formatData(data));
-
   return (
     <div>
       <XYChart height={300} xScale={{ type: "time" }} yScale={{ type: "linear" }}>
-        <Axis orientation="bottom" />
-        <Axis orientation="left" />
-        <LineSeries dataKey="line_01" data={formatData(data)} {...accessors} />
+        <AnimatedAxis orientation="bottom" />
+        <AnimatedAxis orientation="left" />
+        <AnimatedGrid columns={false} />
+        <AnimatedLineSeries dataKey="line_01" data={formatData(data)} {...accessors} />
       </XYChart>
-      <ul>
-        {
-          data.posts.map(post => {
-            const date = new Date();
-            date.setTime(post.createdAt)
-  
-            return(<li key={post.id}>{date.toLocaleString()}</li>)
-          })
-        }
-      </ul>
     </div>
   );
 }
