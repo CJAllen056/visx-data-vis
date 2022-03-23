cbbf88b051430b90dd83ca070f9fb7d5e93cb251
CJAllen056 <christopherjallen056@gmail.com>
2022-03-23 18:59:55 +0000

    attempting to add filter for author also

--- src/chart.js
+++ src/chart.js
@@ -3,7 +3,7 @@ import { XYChart, AnimatedAxis, AnimatedGrid, AnimatedLineSeries, Tooltip } from
 
 import ChartFilter from './filter';
 
-const formatData = (data, filter) => {
+const formatData = (data, filters) => {
   const dates = [
     { x: 'January', y: 0},
     { x: 'February', y: 0},
@@ -20,7 +20,12 @@ const formatData = (data, filter) => {
   ]
 
   data.posts.forEach(post => {
-    if (filter === "All" || filter == post.likelyTopics[0].label) {
+    const topic = post.likelyTopics[0].label;
+    const author = `${post.author.firstName} ${post.author.lastName}`
+
+    if ((filters.topic === "All" || filters.topic === topic)
+      && (filters.author === "All" || filters.author === author)) {
+      console.log(topic, author);
       let date = new Date();
       date.setTime(post.createdAt);    
       dates[date.getMonth()].y++;
@@ -30,21 +35,44 @@ const formatData = (data, filter) => {
   return dates;
 }
 
+const getTopics = data => {
+  return [... new Set(Array.from(data, d => d.likelyTopics[0].label))];
+}
+
+const getAuthors = data => {
+  return [... new Set(Array.from(data, d => `${d.author.firstName} ${d.author.lastName}`))]
+}
+
 const Chart = props => {
   const { data } = props;
-  const [graphData, setGraphData] = useState(formatData(data, "All"));
+  const [topicFilter, setTopicFilter] = useState("All");
+  const [authorFilter, setAuthorFilter] = useState("All");
+  const [graphData, setGraphData] = useState(formatData(data, { topic: topicFilter, author: authorFilter }));
 
   const accessors = {
     xAccessor: (d) => new Date(`${d.x} 2019`),
     yAccessor: (d) => d.y,
   };
 
+  const applyFilter = (setFilterFunc, value) => {
+    setFilterFunc(value);
+    setGraphData(formatData(data, { topic: topicFilter, author: authorFilter }));
+  }
+
   return (
     <div>
-      <ChartFilter
-        options={data.posts[0].likelyTopics}
-        handleChange={e => setGraphData(formatData(data, e.target.value))}
-      />
+      <div className="chart-filters">
+        <ChartFilter
+          filterLabel="topic"
+          options={getTopics(data.posts)}
+          handleChange={e => applyFilter(setTopicFilter, e.target.value)}
+        />
+        <ChartFilter
+          filterLabel="author"
+          options={getAuthors(data.posts)}
+          handleChange={e => applyFilter(setAuthorFilter, e.target.value)}
+        />
+      </div>
       <XYChart height={300} xScale={{ type: "time" }} yScale={{ type: "linear" }}>
         <AnimatedAxis orientation="bottom" />
         <AnimatedAxis orientation="left" />
--- src/filter.js
+++ src/filter.js
@@ -1,13 +1,13 @@
 const ChartFilter = props => {
-  const { options, handleChange } = props;
+  const { filterLabel, options, handleChange } = props;
 
   return (
     <div>
-      <label htmlFor="topics-selection">Filter by topic: </label>
+      <label htmlFor="topics-selection">Filter by {filterLabel}: </label>
       <select id="topics-selection" onChange={e => handleChange(e)}>
         <option value="All">Show All</option>
-        {options.map(topic => {
-          return <option key={topic.label} value={topic.label}>{topic.label}</option>
+        {options.map(option => {
+          return <option key={option} value={option}>{option}</option>
         })}
       </select>
     </div>
